4882dc99f2e8430cb0a709320575add33e0782ed
Monia Favaro <moniafavaro@hotmail.com>
2021-10-12 16:31:36 +0100

    added more enemys, high-score functionality, life functionality and music

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,16 @@ const game = document.querySelector('.game')
 const width = 21
 const gridCellCount = width * width
 const playerStartPosition = 325
-const enemyStartPosition = 241
+const enemyStartPosition = 199
 let playerIndex = 0
-let enemyIndex = 0
+let enemyAmount = 3
+let enemyIndex = []
+const lifeElement = document.querySelector('.life-span')
 let life = 3
+const scoreElement = document.querySelector('.score-span')
 let score = 0
-let highScore = 0
+const highScoreElement = document.querySelector('#highScores')
+let highScore = localStorage.getItem('highScore')
 
 const MOVE_UP = -21
 const MOVE_RIGHT = 1
@@ -26,8 +30,8 @@ const level = [
     [0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0],
     [0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
     [0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0],
-    [0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0],
-    [0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
     [0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0],
     [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
     [0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0],
@@ -72,6 +76,8 @@ function play() {
         let player = document.getElementById('sqm-' + playerStartPosition)
         player.classList.add('sqm-player')
         playerIndex = playerStartPosition
+        scoreElement.innerHTML = score
+        lifeElement.innerHTML = life
     }
 
     const arrowUp = () => {
@@ -98,12 +104,12 @@ function play() {
         tryMove(playerIndex, MOVE_LEFT, playerOnLeft, isPlayer)
     }
     
-    const tryMove = (currentIndex, changeIndex, indexAtLimit, isPlayer) => {
+    const tryMove = (currentIndex, changeIndex, indexAtLimit, isPlayer, enemyPosition = 0) => {
         const newIndex = currentIndex + changeIndex
         if (cannotMove(currentIndex, newIndex, indexAtLimit)) {
             return false
         }
-        move(currentIndex, newIndex, isPlayer)
+        move(currentIndex, newIndex, isPlayer, enemyPosition)
         return true
     }
     
@@ -119,18 +125,45 @@ function play() {
         return false
     }
     
-    function move(currentIndex, newIndex, isPlayer) {
+    function move(currentIndex, newIndex, isPlayer, enemyPosition) {
         if (isPlayer) {
             cells[currentIndex].classList.remove('sqm-player')
             cells[newIndex].classList.add('sqm-player')
-            cells[newIndex].classList.remove('sqm-food')
+            if (cells[newIndex].classList.contains('sqm-food')) {
+                cells[newIndex].classList.remove('sqm-food')
+                score += 5
+                scoreElement.innerHTML = score
+            }
             playerIndex = newIndex
         } else {
             cells[currentIndex].classList.remove('sqm-enemy')
-            cells[currentIndex].classList.add('sqm-food')
             cells[newIndex].classList.add('sqm-enemy')
-            cells[newIndex].classList.remove('sqm-food')
-            enemyIndex = newIndex
+            enemyIndex[enemyPosition] = newIndex
+        }
+        gameOver()
+    }
+
+    function gameOver() {
+        let isGameOver = false
+        for (let i = 0; i < enemyAmount; i++) {
+            if (playerIndex === enemyIndex[i]) {
+                if (life > 1) {
+                    life--
+                    lifeElement.innerHTML = life
+                    cells[playerIndex].classList.remove('sqm-player')
+                    let player = document.getElementById('sqm-' + playerStartPosition)
+                    player.classList.add('sqm-player')
+                    playerIndex = playerStartPosition
+                } else {
+                    if (score > highScore) {
+                        localStorage.setItem('highScore', score)
+                        highScoreElement.innerHTML = score
+                        highScore = localStorage.getItem('highScore')
+                    }
+                    isGameOver = true
+                }
+                alert('Game Over')
+            }
         }
     }
     
@@ -153,46 +186,50 @@ function play() {
 
     // enemy functions
     function enemyStart() {
-        let enemy = document.getElementById('sqm-' + enemyStartPosition)
-        enemy.classList.add('sqm-enemy')
-        enemyIndex = enemyStartPosition
+        for (let i = 0; i < enemyAmount; i++) {
+            let enemy = document.getElementById('sqm-' + enemyStartPosition)
+            enemy.classList.add('sqm-enemy')
+            enemyIndex.push(enemyStartPosition)
+        }
     }
 
     function enemyMove() {
-        if (movements.length === 0) {
-            movements = [MOVE_UP, MOVE_RIGHT, MOVE_DOWN, MOVE_LEFT]
-        }
-
-        let randomMovement = movements[Math.floor(Math.random() * movements.length)]
-        
-        let isPlayer = false
-        const alwaysFalse = (index) => index !== index
-        let moved = tryMove(enemyIndex, randomMovement, alwaysFalse, isPlayer)
-        if (!moved) {
-            const index = movements.indexOf(randomMovement)
-            if (index >= 0) {
-                movements.splice(index, 1)
+        for (let i = 0; i < enemyAmount; i++) {
+            if (movements.length === 0) {
+                movements = [MOVE_UP, MOVE_RIGHT, MOVE_DOWN, MOVE_LEFT]
             }
-        } else {
-            let oldPosition = 0
-            switch(randomMovement) {
-                case MOVE_UP:
-                    oldPosition = MOVE_DOWN
-                    break
-                case MOVE_RIGHT:
-                    oldPosition = MOVE_LEFT
-                    break
-                case MOVE_DOWN:
-                    oldPosition = MOVE_UP
-                    break
-                case MOVE_LEFT:
-                    oldPosition = MOVE_RIGHT
-                    break
-            }
-            movements = [MOVE_UP, MOVE_RIGHT, MOVE_DOWN, MOVE_LEFT]
-            const index = movements.indexOf(oldPosition)
-            if (index >= 0) {
-                movements.splice(index, 1)
+            
+            let randomMovement = movements[Math.floor(Math.random() * movements.length)]
+            
+            let isPlayer = false
+            const alwaysFalse = (index) => index !== index
+            let moved = tryMove(enemyIndex[i], randomMovement, alwaysFalse, isPlayer, i)
+            if (!moved) {
+                const index = movements.indexOf(randomMovement)
+                if (index >= 0) {
+                    movements.splice(index, 1)
+                }
+            } else {
+                let oldPosition = 0
+                switch(randomMovement) {
+                    case MOVE_UP:
+                        oldPosition = MOVE_DOWN
+                        break
+                    case MOVE_RIGHT:
+                        oldPosition = MOVE_LEFT
+                        break
+                    case MOVE_DOWN:
+                        oldPosition = MOVE_UP
+                        break
+                    case MOVE_LEFT:
+                        oldPosition = MOVE_RIGHT
+                        break
+                }
+                movements = [MOVE_UP, MOVE_RIGHT, MOVE_DOWN, MOVE_LEFT]
+                const index = movements.indexOf(oldPosition)
+                if (index >= 0) {
+                    movements.splice(index, 1)
+                }
             }
         }
     }
@@ -213,7 +250,10 @@ function play() {
     setInterval(enemyMove, 400)
 }
 
-
+if (highScore === null) {
+    highScore = 0
+}
 creatGrid()
 createLevel()
 document.getElementById('start-button').addEventListener('click', play)
+highScoreElement.innerHTML = highScore
